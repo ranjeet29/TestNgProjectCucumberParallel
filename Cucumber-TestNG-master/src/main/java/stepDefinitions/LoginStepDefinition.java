37efbbb2aa34e9d7ffe077f0dded957e9dec0dae
Ranjeet Kumar <ranjeet.kumar@innovacer.com>
2020-02-16 21:47:51 +0530

    screen

--- Cucumber-TestNG-master/src/main/java/stepDefinitions/LoginStepDefinition.java
+++ Cucumber-TestNG-master/src/main/java/stepDefinitions/LoginStepDefinition.java
@@ -17,6 +17,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import cucumber.api.java.After;
 import cucumber.api.Scenario;
 import gherkin.formatter.model.Step;
 import cucumber.api.java.Before;
