03ed53b48f5efc1bb2844ab78a60b139142e3ac1
Ranjeet Kumar <ranjeet.kumar@innovacer.com>
2020-02-16 23:34:56 +0530

    screen shot

--- Cucumber-TestNG-master/src/main/java/stepDefinitions/LoginStepDefinition.java
+++ Cucumber-TestNG-master/src/main/java/stepDefinitions/LoginStepDefinition.java
@@ -6,6 +6,7 @@ import java.net.URL;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,13 +14,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 
-import cucumber.api.java.After;
+
 import cucumber.api.Scenario;
 import gherkin.formatter.model.Step;
+import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -35,9 +34,8 @@ public class LoginStepDefinition{
 	 @Before
 	 public void beforeTest(Scenario scenario){
 		   this.scenario = scenario;
-		   long threadId = Thread.currentThread().getId();
 		    String processName = ManagementFactory.getRuntimeMXBean().getName();
-		    System.out.println("Started in thread: " + threadId + ", in JVM: " + processName);
+		    System.out.println("Started in thread:  in JVM: " + processName);
 	 }
 
 	
@@ -45,14 +43,14 @@ public class LoginStepDefinition{
 	 public void user_already_on_login_page() throws MalformedURLException{
 	 System.setProperty("webdriver.chrome.driver","chromedriver");
 	 ChromeOptions options = new ChromeOptions();
-	 options.addArguments("start-maximized"); // https://stackoverflow.com/a/26283818/1689770
-     options.addArguments("enable-automation"); // https://stackoverflow.com/a/43840128/1689770
-     options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
-     options.addArguments("--headless");
-     options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
-     options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
-     options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
-     options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+	 options.addArguments("start-maximized");
+	 options.addArguments("--always-authorize-plugins");
+     options.addArguments("enable-automation"); 
+    // options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+    // options.addArguments("--headless");
+   //  options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+   //  options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+   //  options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
      driver = new ChromeDriver(options);
 
 	 // DesiredCapabilities capabilities = new DesiredCapabilities().chrome();
@@ -77,21 +75,22 @@ public class LoginStepDefinition{
 	 }
 	
 
-	 @Then("^Close the browser$")
-	 public void close_the_browser(){
-		 driver.quit();
-	 }
-	
 	
 	 @After
-	 public void tearDown() {
-		 if (scenario.isFailed()) {
-		      // Take a screenshot...
-		      final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-		      scenario.embed(screenshot, "image/png"); // ... and embed it in the report.
-		    }
+	 public void tearDown(Scenario scenario) {
+		 if(scenario.isFailed()) {
+			 final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+			   // embed it in the report.
+			      scenario.embed(screenshot, "image/png");     
+	     }
+	    if(driver != null) {
+	    	driver.quit();
+	    	
+	    }
 	 }
-	 
+	
+	
+    
 	 
 		public void afterTest(Scenario scenario) throws InterruptedException {
 		    System.out.println("Taking screenshot IF Test Failed (sysOut)");
